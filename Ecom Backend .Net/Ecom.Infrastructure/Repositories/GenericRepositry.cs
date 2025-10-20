6cb08cd2c3330f23546436198a4a36e691b95a42
Muhammed Reda <moreda263@gmail.com>
2025-10-21 02:51:48 +0300

    Initial Commit

---
+++ Ecom Backend .Net/Ecom.Infrastructure/Repositories/GenericRepositry.cs
@@ -0,0 +1,75 @@
+﻿using Ecom.Core.Interfaces;
+using Ecom.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Repositories
+{
+    public class GenericRepositry<T> : IGenericRepositry<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        public GenericRepositry(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        => await _context.Set<T>().AsNoTracking().ToListAsync();
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] Includes)
+        {
+            var query = _context.Set<T>().AsQueryable();
+
+            //apply include
+            foreach (var item in Includes)
+            {
+                query = query.Include(item);
+            }
+            return await query.ToListAsync();
+        }
+        public async Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _context.Set<T>();
+
+            // Apply includes
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+
+            // Filter by Id
+            var entity = await query.FirstOrDefaultAsync(x => EF.Property<int>(x, "Id") == id);
+            return entity;
+        }
+
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);   
+            await _context.SaveChangesAsync();
+        }
+    }
+}
