fb962d95ff5600c196478b84be6f04ba1cfe7f81
Muhammed Reda <moreda263@gmail.com>
2025-11-04 16:49:29 +0200

    added lazyLoading

--- Ecom Frontend Angular/src/app/app-routing-module.ts
+++ Ecom Frontend Angular/src/app/app-routing-module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Home } from './home/home';
-import { ShopModule } from './shop/shop-module';
-import { ShopComponent } from './shop/shop';
-import { ProductDetails } from './shop/product-details/product-details';
+
 
 const routes: Routes = [
   {path: '', component: Home},
-  {path: 'shop', component: ShopComponent},
-  {path: 'product-details/:id', component: ProductDetails},
+  { path: 'shop', loadChildren: () => import('./shop/shop-module').then(m => m.ShopModule) },
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
