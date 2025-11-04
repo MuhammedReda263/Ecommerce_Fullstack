933686a755a584fa90a86d8630c04cf9f488acc7
Muhammed Reda <moreda263@gmail.com>
2025-11-04 08:00:13 +0200

    added routing

--- Ecom Frontend Angular/src/app/app-routing-module.ts
+++ Ecom Frontend Angular/src/app/app-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { ShopModule } from './shop/shop-module';
+import { ShopComponent } from './shop/shop';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: Home},
+  {path: 'shop', component: ShopComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
