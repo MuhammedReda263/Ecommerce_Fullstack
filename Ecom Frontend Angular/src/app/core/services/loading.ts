db79b74968e40e7d714bd2a8d3d7859bd45f3a88
Muhammed Reda <moreda263@gmail.com>
2025-11-06 11:24:36 +0200

    added toaster

---
+++ Ecom Frontend Angular/src/app/core/services/loading.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Loading implements OnInit {
+   constructor(private spinner: NgxSpinnerService) {}
+
+  ngOnInit() {
+    /** spinner starts on init */
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 5000);
+  }
+  
+}
