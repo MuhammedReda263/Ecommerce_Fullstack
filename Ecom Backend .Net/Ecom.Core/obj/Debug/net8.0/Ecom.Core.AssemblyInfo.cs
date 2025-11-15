5672eb36ea85592a51d874a6b4d926958c5bd6ad
Muhammed Reda <moreda263@gmail.com>
2025-11-15 22:24:23 +0200

    Create AuthRepository with Register Logic

--- Ecom Backend .Net/Ecom.Core/obj/Debug/net8.0/Ecom.Core.AssemblyInfo.cs
+++ Ecom Backend .Net/Ecom.Core/obj/Debug/net8.0/Ecom.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecom.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edbaf5cc6f3fb875255cfa96ee68671d1b827266")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+482143a37032154e5582358b80123a4552c052a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecom.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecom.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
